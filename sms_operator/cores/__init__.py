8800c842b2fc8bf3d2ab05304e8acebcffd8d550
Lubos Matl <matllubos@gmail.com>
2015-06-01 22:02:47 +0200

    v0.0.4 Fixed core

--- sms_operator/cores/__init__.py
+++ sms_operator/cores/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 from is_core.main import UIRestModelISCore
 
@@ -29,6 +30,12 @@ class SMSTemplateIsCore(UIRestModelISCore):
     abstract = True
     form_fields = ('slug', 'body')
     form_readonly_fields = ('slug',)
+
+    def get_form_fields(self, request, obj=None):
+        form_fields = ['slug']
+        for language_code, _ in settings.LANGUAGES:
+            form_fields.append('body_%s' % language_code)
+        return form_fields
 
     def has_create_permission(self, request, obj=None):
         return False
